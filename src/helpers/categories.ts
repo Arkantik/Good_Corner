02f9e307c219c59e5444b6495fefdb8f38641d22
Jeremy Dohin <arkantik.pro@gmail.com>
2023-11-20 16:57:06 +0100

    Init of Good Corner frontend

---
+++ src/helpers/categories.ts
@@ -0,0 +1,59 @@
+interface CategoryProps {
+  link: string;
+  title: string;
+}
+
+export const categories: CategoryProps[] = [
+  {
+    link: "",
+    title: "Ameublement",
+  },
+  {
+    link: "",
+    title: "Électroménager",
+  },
+  {
+    link: "",
+    title: "Photographie",
+  },
+  {
+    link: "",
+    title: "Informatique",
+  },
+  {
+    link: "",
+    title: "Téléphonie",
+  },
+  {
+    link: "",
+    title: "Vélos",
+  },
+  {
+    link: "",
+    title: "Véhicules",
+  },
+  {
+    link: "",
+    title: "Sport",
+  },
+  {
+    link: "",
+    title: "Habillement",
+  },
+  {
+    link: "",
+    title: "Bébé",
+  },
+  {
+    link: "",
+    title: "Outillage",
+  },
+  {
+    link: "",
+    title: "Services",
+  },
+  {
+    link: "",
+    title: "Vacances",
+  },
+];
