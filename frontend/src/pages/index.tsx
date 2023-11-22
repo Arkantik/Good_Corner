ea9cf2164bcfcb50b5108d641af846677446cf13
Jeremy Dohin <arkantik.pro@gmail.com>
2023-11-22 17:04:18 +0100

    Moved api call to index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,40 @@
 import RecentAds from "@/components/RecentAds";
 import { Ad, RecentAdsProps } from "@/interfaces/ads";
 import Layout from "@/layouts/Layout";
+import axios from "axios";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [filteredAds, setFilteredAds] = useState<Ad[]>();
+  const [ads, setAds] = useState<Ad[]>([]);
+  const [filteredAds, setFilteredAds] = useState<Ad[]>([]);
 
   const handleSearch = (query: string) => {
-    const filtered = ads.filter(ad =>
-      ad.title.toLowerCase().includes(query.toLowerCase())
-    );
+    const filtered = query
+      ? ads.filter((ad) =>
+        ad.title.toLowerCase().includes(query.toLowerCase())
+      )
+      : ads;
     setFilteredAds(filtered);
   };
 
+  const fetchAds = () => {
+    axios
+      .get<Ad[]>("http://localhost:4000/ads")
+      .then((res) => {
+        setAds(res.data);
+        setFilteredAds(res.data);
+      })
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
+
   return (
-    <Layout pageTitle="Acceuil" onSearch={handleSearch}>
-      <RecentAds ads={filteredAds} />
+    <Layout pageTitle="Accueil" onSearch={handleSearch}>
+      <RecentAds filteredAds={filteredAds} />
     </Layout>
   );
 }
