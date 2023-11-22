aa84340f8e378e4ab915abf14f39e731013526ec
Jeremy Dohin <arkantik.pro@gmail.com>
2023-11-22 16:57:55 +0100

    Converted AdCard into clickable button

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export type AdCardProps = {
@@ -8,16 +11,18 @@ export type AdCardProps = {
   link: string;
 };
 
-export default function AdCard({ title, price, picture, link }: AdCardProps) {
+export default function AdCard({ id, title, price, picture, link }: AdCardProps) {
+  const router = useRouter();
+
   return (
-    <div className="ad-card-container">
+    <button className="ad-card-container" onClick={() => router.push(`/ads/${id}`)}>
       <a className="ad-card-link" href={link}>
         <img className="ad-card-image" src={picture} />
         <div className="ad-card-text">
           <div className="ad-card-title">{title}</div>
-          <div className="ad-card-price">{price} €</div>
+          <div className="ad-card-price">{price}€</div>
         </div>
       </a>
-    </div>
+    </button>
   );
 }
