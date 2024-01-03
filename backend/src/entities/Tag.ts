20a902b79183ce2c915b67260a192ab0b1642320
Jeremy Dohin <arkantik.pro@gmail.com>
2024-01-03 11:10:18 +0100

    Update of entities to use type-graphql

--- backend/src/entities/Tag.ts
+++ backend/src/entities/Tag.ts
@@ -7,15 +7,26 @@ import {
   ManyToMany,
 } from "typeorm";
 import Ad from "./Ad";
+import { Field, InputType, ObjectType } from "type-graphql";
 
 @Entity()
+@ObjectType()
 export default class Tag extends BaseEntity {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column({ length: 100 })
   name: string;
 
+  @Field(() => [Ad])
   @ManyToMany(() => Ad, (ad) => ad.tags)
   ads: Ad[];
+}
+
+@InputType()
+export class TagInput {
+  @Field()
+  name: string;
 }
