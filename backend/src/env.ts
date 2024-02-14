00c036afbc411ff651a61befe4c076537e06aec8
Jeremy Dohin <arkantik.pro@gmail.com>
2024-02-14 16:25:51 +0100

    Update of env file

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,9 +1,16 @@
 import { load } from "ts-dotenv";
 
+// https://github.com/LeoBakerHytch/ts-dotenv
+
 export default load({
-  DB_HOST: String,
-  DB_PORT: Number,
-  DB_USER: String,
-  DB_PASS: String,
-  DB_NAME: String,
+  JWT_PRIVATE_KEY: String,
+  CORS_ALLOWED_ORIGINS: String,
+  NODE_ENV: ["production" as const, "development" as const, "test" as const],
+  SERVER_HOST: { type: String, optional: true, default: "localhost" },
+  SERVER_PORT: { type: Number, optional: true, default: 4001 },
+  DB_HOST: { type: String, optional: true, default: "db" },
+  DB_PORT: { type: Number, optional: true, default: 5432 },
+  DB_USER: { type: String, optional: true, default: "postgres" },
+  DB_PASS: { type: String, optional: true, default: "postgres" },
+  DB_NAME: { type: String, optional: true, default: "postgres" },
 });
