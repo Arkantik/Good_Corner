71facd952cd50fcdba2958942aa55a22c9fb4cbd
Jeremy Dohin <arkantik.pro@gmail.com>
2024-03-21 15:14:29 +0100

    Creation of pages and components for user auth

---
+++ frontend/src/pages/me.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/layouts/Layout";
+import Profile from "@/components/Profile";
+
+export default function ProfilePage() {
+    return (
+        <Layout pageTitle="Mon profil - TGC">
+            <Profile />
+        </Layout>
+    );
+}
